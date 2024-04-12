d814530054a118595e3f87bdbc4ee3e6d9e3cab1
김강민 <144314311+VERUS2621@users.noreply.github.com>
2024-04-12 13:00:37 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name = 'statistools',
+    version='0.0.1',
+    description='useful tool',
+    author = 'kangmin',
+    url = '',
+    download_url='',
+    packagers = [ 'statis' ],
+    classifiers = [
+      "Programming Language :: Python :: 3"
+    ]
+)
