31fb1d2df1a1376cc18bcd58a1a20eed9d429647
김강민 <144314311+VERUS2621@users.noreply.github.com>
2024-04-12 16:32:00 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ import setuptools
 
 setuptools.setup(
     name = 'statstools',
-    version='0.0.1',
+    version='0.0.2',
     description='useful tool',
     author = 'kangmin',
     url = 'https://github.com/VERUS2621/statis0001',
     download_url='https://github.com/VERUS2621/statis0001',
-    packagers = [ 'stats' ],
+    packagers = [ 'stats01' ],
     classifiers = [
       "Programming Language :: Python :: 3"
     ]
