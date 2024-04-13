d0fa8437e98c52b77060a5adc36a0d89a5e21a1f
김강민 <144314311+VERUS2621@users.noreply.github.com>
2024-04-13 23:23:37 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import setuptools
 
 setuptools.setup(
     name = 'spstats',
-    version='0.0.2',
-    description ='useful tool',
+    version='0.0.5',
+    description ='usefule tools, exist error',
     author = 'kangmin',
     url = 'https://github.com/VERUS2621/statis0001',
     download_url='https://github.com/VERUS2621/statis0001',
