767e144c26636612b0b4c945177e9fd4d36c43e0
김강민 <144314311+VERUS2621@users.noreply.github.com>
2024-04-13 16:25:04 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name = 'statstools',
     version='0.0.2',
-    description='useful tool',
+    description ='useful tool',
     author = 'kangmin',
     url = 'https://github.com/VERUS2621/statis0001',
     download_url='https://github.com/VERUS2621/statis0001',
